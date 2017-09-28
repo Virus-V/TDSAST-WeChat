57e08ef7cdc32d083e44b16a01020befa669c005
Virus.V <virusv@qq.com>
2017-09-28 14:56:11 +0800

    配置成测试号，开始调试

--- index.php
+++ index.php
@@ -9,10 +9,17 @@ define('APP_PATH',dirname(__FILE__));
 define('VPF_PATH',dirname(APP_PATH).'/vpf');
 require VPF_PATH.'/vpf.php';
 
+// 科协微信公众号（个人主体）
+// define('WCHAT_TOKEN','e916e7eb18b20c399307e2d1e8971faa');
+// define('WCHAT_ENCODINGAESKEY','HRxyFWKxK9vOVg80jizyl6Fzr8XCmxNw8jxeMDvPJCm');
+// define('WCHAT_APPID','wx5e7976716f37e2d3');
+// define('WCHAT_APPSECRET','ae313bf8d6b3d084d4a5404c41c667fd');
+
+// 我的测试号
 define('WCHAT_TOKEN','e916e7eb18b20c399307e2d1e8971faa');
 define('WCHAT_ENCODINGAESKEY','HRxyFWKxK9vOVg80jizyl6Fzr8XCmxNw8jxeMDvPJCm');
-define('WCHAT_APPID','wx5e7976716f37e2d3');
-define('WCHAT_APPSECRET','ae313bf8d6b3d084d4a5404c41c667fd');
+define('WCHAT_APPID','wx13eb64b80cf71a83');
+define('WCHAT_APPSECRET','97317fb3699390f4caa444615f30115e');
 
 $app = new Vpf\App(); /* 创建控制器实例 */
 $app->run();
