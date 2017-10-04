77d45af740b52f1c5c2647758df51e6ca0c2cdd7
Virus.V <virusv@qq.com>
2017-10-04 13:01:59 +0800

    在navigation上增加了动画效果

--- tpl/default/navigation.php
+++ tpl/default/navigation.php
@@ -1,17 +1,19 @@
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top animatedParent animateOnce" data-sequence="200">
   <div class="container-fluid"> 
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-      <a class="navbar-brand" href="#">TD-Cloud</a></div>
+      <a class="navbar-brand animated init" data-id="1" data-effect-in="bounceIn" href="#">TD-Cloud</a>
+    </div>
 	  
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="topFixedNavbar1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">情怀</a></li>
-        <li><a href="#">团队</a></li>
-        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">通达云<span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
+        <li class="active animated init" data-id="2" data-effect-in="flipInX"><a href="#">情怀</a></li>
+        <li><a href="#" class="animated init" data-id="3" data-effect-in="flipInX">团队</a></li>
+        <li class="dropdown animated init" data-id="4" data-effect-in="flipInX">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">通达云<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu" >
               <li><a href="#">介绍</a></li>
               <li><a href="#">架构</a></li>
               <li role="separator" class="divider"></li>
